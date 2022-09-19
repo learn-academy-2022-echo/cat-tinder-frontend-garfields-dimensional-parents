03c95990fae3d0effdec66f1ffb72602859b5451
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-09-19 13:46:34 -0700

    Directories made, pages and components made, everything works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import CatNew from './pages/CatNew'
+import CatEdit from './pages/CatEdit'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App
+      <Header/>
+      <Home/>
+      <CatIndex/>
+      <CatShow/>
+      <CatNew/>
+      <CatEdit/>
+      <NotFound/>
+      <Footer/>
+
+
+
+      
     </div>
   );
 }
---
+++ src/pages/CatIndex.js
@@ -0,0 +1,13 @@
+// Imports
+import React from 'react'
+
+// Component declaration 
+const CatIndex = () => {
+
+  // JSX Return
+  return (
+    <div> CatIndex </div>
+  )
+}
+
+export default CatIndex
