bef5a5887a069240ee6f534dbe29e5fddeefaea3
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-09-19 14:06:17 -0700

    routes for App.js all working

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import CatNew from './pages/CatNew'
 import CatEdit from './pages/CatEdit'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import { Routes, Route } from "react-router-dom"
 
 
 
@@ -17,17 +18,16 @@ function App() {
     <div>
       App
       <Header/>
-      <Home/>
-      <CatIndex/>
-      <CatShow/>
-      <CatNew/>
-      <CatEdit/>
-      <NotFound/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/catindex" element={<CatIndex />} />
+        <Route path="/catshow" element={<CatShow />} />
+        <Route path="/catnew" element={<CatNew />} />
+        <Route path="/catedit" element={<CatEdit />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
       <Footer/>
 
-
-
-      
     </div>
   );
 }
